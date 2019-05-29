89a7e399c774a50ba27dbe97aceeed72c35069b4
ganeshpixcity <ganesh@pix.city>
2019-05-29 19:14:41 +0530

    Adding e-mail ID duplicate check

--- assets/b2b/js/client_registration.js
+++ assets/b2b/js/client_registration.js
@@ -1,20 +1,24 @@
-var $form = $("form[name='client']");
+
+var $form = $('form[name="client"]');
 
 $form.validate({
     rules:{
-        "client[email]": {required: true, maxlength: 30, email: true},
-        "client[firstName]": {required: true, maxlength: 30},
-        "client[lastName]": {required: true, maxlength: 30},
-        "client[company]": {required: true, maxlength: 50},
-        "client[clientInfo][siret]": {required: true, maxlength: 14, minlength: 14, number: true},
-        "client[clientInfo][address]": {required: true, maxlength: 50},
-        "client[clientInfo][city]": {required: true, maxlength: 54},
-        "client[clientInfo][companyType]": {required: true, maxlength: 50},
-        "client[clientInfo][postalCode]": {required: true, number: true, minlength: 5, maxlength: 5}
+        "client[email]":{required: true, email: true, remote: '/client/check-email'},
+        "client[firstName]":{required: true, maxlength: 30},
+        "client[lastName]":{required: true, maxlength: 30},
+        "client[company]":{required: true, maxlength: 50},
+        "client[password]":{required: true, maxlength: 30},
+        "client[clientInfo][siret]":{required: true, maxlength: 14, minlength:14, number: true},
+        "client[clientInfo][address]":{required: true, maxlength: 50},
+        "client[clientInfo][postalCode]":{required: true, maxlength: 5, minlength:5, number: true},
+        "client[clientInfo][city]":{required: true, maxlength: 50},
+        "client[clientInfo][companyType]":{required: true, maxlength: 50},
+        "client[plainPassword][first]": {minlength: 8, password: true},
+        "client[plainPassword][second]": {minlength: 8, password: true, equalTo: "#client_plainPassword_first"}
+    },
+    messages: {
+        "client[email]": {
+            remote: "This e-mail is already in use. Please try another e-mail!"
+        }
     }
-});
-
-
-$('.btn-success').on('click', function(){
-    $form.valid();
-});
+})
