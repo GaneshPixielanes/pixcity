fe9b49a6de2570fbcf0ad1057ada07e5de9cf7da
azim <azim@pix.city>
2019-06-03 19:02:35 +0530

    empty messages

--- src/Controller/B2B/Client/EmailController.php
+++ src/Controller/B2B/Client/EmailController.php
@@ -129,7 +129,7 @@ class EmailController extends AbstractController
 
         $tickit_data = $ticketRepository->find($id);
 
-        if($tickit_data->getMessages == 0 and empty($tickit_data->getMessages)){
+        if(empty($tickit_data->getMessages)){
             foreach ($tickit_data->getMessages() as $data){
                 foreach ($data as $item) {
                     $item->setStatus(0);
