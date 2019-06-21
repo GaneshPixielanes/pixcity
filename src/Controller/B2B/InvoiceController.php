117024155f1cb51d2363fb08a2dd6dadc70ebde8
Ganesh <ganesh@pix.city>
2019-06-21 16:59:58 +0530

    Invoice page bug fix

--- src/Controller/B2B/InvoiceController.php
+++ src/Controller/B2B/InvoiceController.php
@@ -4,6 +4,7 @@ namespace App\Controller\B2B;
 
 use App\Constant\MissionStatus;
 use App\Repository\MissionRepository;
+use App\Repository\OptionRepository;
 use App\Repository\UserMissionRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
@@ -15,7 +16,7 @@ class InvoiceController extends AbstractController
     /**
      * @Route("list", name="list")
      */
-    public function index(UserMissionRepository $missionRepo)
+    public function index(UserMissionRepository $missionRepo, OptionRepository $optionsRepo)
     {
         $missions = $missionRepo->findBy([
            'user' => $this->getUser(),
@@ -24,6 +25,7 @@ class InvoiceController extends AbstractController
 
         return $this->render('b2b/invoice/index.html.twig', [
             'missions' => $missions,
+            'tax' => $optionsRepo->findOneBy(['slug' => 'margin'])
         ]);
     }
 
