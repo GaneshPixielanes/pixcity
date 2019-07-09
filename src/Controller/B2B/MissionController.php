05b407eafc29d4ea8a6d37e4d0386bb8ac85915e
azim <azim@pix.city>
2019-07-09 21:03:13 +0530

    Confirmation modal added in mission edit

--- src/Controller/B2B/MissionController.php
+++ src/Controller/B2B/MissionController.php
@@ -217,6 +217,7 @@ class MissionController extends AbstractController
         $form = $this->createForm(MissionLogType::class, $missionLog);
 
         $form->handleRequest($request);
+
         if($form->isSubmitted())
         {
 
