a3e122c19910714e472ade8f38f1e1d33e37ce19
ganeshpixcity <ganesh@pix.city>
2019-07-15 21:08:27 +0530

    cm list update

--- src/Controller/Admin/Pages/AdminUsersController.php
+++ src/Controller/Admin/Pages/AdminUsersController.php
@@ -164,7 +164,7 @@ class AdminUsersController extends Controller
                 $query = $em->createQuery('SELECT g, COUNT(m.user) AS userPack
                     FROM App:User AS g
                     LEFT JOIN App:UserPacks AS m WITH g.id = m.user
-                    WHERE g.active=1 AND g.deleted = 0 AND g.cm_upgrade_b2b_date is not null GROUP BY g.id');
+                    WHERE g.active=1 AND g.deleted = 0 AND g.cmUpgradeB2bDate is not null GROUP BY g.id');
                 $result =  $query->getResult();
 
                 //$list = $userRepository->findBy([], ['createdAt' => 'DESC']);
