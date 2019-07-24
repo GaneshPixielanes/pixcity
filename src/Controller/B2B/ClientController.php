7b06e128c1fd357e4b4dd324229854a0b252fa99
azim <azim@pix.city>
2019-07-24 17:53:02 +0530

    Invices

--- src/Controller/B2B/ClientController.php
+++ src/Controller/B2B/ClientController.php
@@ -166,7 +166,7 @@ class ClientController extends Controller
 
         $client_filename = 'PX-'.$mission->getId().'-'.$mission->getActiveLog()->getId()."-client.pdf";
 
-        $result = '/invoices/'.$mission->getId().'/'.$client_filename;
+        $result = 'http://docs.google.com/gview?url=/invoices/'.$mission->getId().'/'.$client_filename;
 
         return new JsonResponse($result);
 
