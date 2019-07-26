95e5d1b1da0b26463760fb6bf896f7cef6f03612
azim <azim@pix.city>
2019-07-27 04:07:35 +0530

    refund

--- src/Service/MangoPayService.php
+++ src/Service/MangoPayService.php
@@ -115,10 +115,10 @@ class MangoPayService
         $Refund->AuthorId = $transaction[0]->getMangopayUserId();
         $Refund->DebitedFunds = $this->mangoPayMoney;
         $Refund->DebitedFunds->Currency = "EUR";
-        $Refund->DebitedFunds->Amount = $amount;
+        $Refund->DebitedFunds->Amount = $refund_amount;
         $Refund->Fees = $this->mangoPayMoney;
         $Refund->Fees->Currency = "EUR";
-        $Refund->Fees->Amount = -$refund_amount;
+        $Refund->Fees->Amount = $refund_amount;
 
         $reponse = $this->mangoPayApi->PayIns->CreateRefund($PayInId, $Refund);
 
