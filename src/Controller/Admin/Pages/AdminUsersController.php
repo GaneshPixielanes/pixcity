af7f8afffe8a6d796a674397ce6985001f3840e3
bholasinghs <singh.b78@gmail.com>
2019-08-02 16:09:40 +0530

    Cm list controller and cmlist twig page

--- src/Controller/Admin/Pages/AdminUsersController.php
+++ src/Controller/Admin/Pages/AdminUsersController.php
@@ -167,7 +167,7 @@ class AdminUsersController extends Controller
                 $query = $em->createQuery('SELECT g, COUNT(m.user) AS userPack
                     FROM App:User AS g
                     LEFT JOIN App:UserPacks AS m WITH g.id = m.user
-                    WHERE g.active=1 AND g.deleted = 0 AND g.cmUpgradeB2bDate is not null GROUP BY g.id');
+                    WHERE g.active=1 AND g.deleted = 0 AND g.cmUpgradeB2bDate is not null GROUP BY g.id ORDER BY g.id DESC');
                 $result =  $query->getResult();
 
                 //$list = $userRepository->findBy([], ['createdAt' => 'DESC']);
