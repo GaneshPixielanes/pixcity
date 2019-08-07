f779e683b12888f6f3612e3b75d7a72d934243dd
azim <azim@pix.city>
2019-08-07 21:09:52 +0530

    ->where('u.b2b_cm_approval = :approval')             ->setParameter('approval', 1)

--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -134,7 +134,8 @@ class UserRepository extends ServiceEntityRepository
             ->orderBy('u.id','DESC')
 
             ->groupBy('u.id')
-            ->where('u.b2b_cm_approval = 1')
+            ->where('u.b2b_cm_approval = :approval')
+            ->setParameter('approval', 1)
 //            ->where('u.deleted IS NULL OR u.deleted = 0')
         ;
         $qb = $this->_applyFiltersClients($qb, $filters)
@@ -156,9 +157,10 @@ class UserRepository extends ServiceEntityRepository
 //            ->leftJoin('u.links', 'c')
 //            ->leftJoin('u.favoriteCategories', 'category')
             //->innerJoin('u.pixie', 'p')
-            ->innerJoin('u.userPacks','packs')
+            ->innerJoin('u.userPacks', 'd')
             ->leftJoin('u.userRegion', 'r')
-            ->orWhere('u.b2b_cm_approval = 1')
+            ->where('u.b2b_cm_approval = :approval')
+            ->setParameter('approval', 1)
 //            ->where('u.deleted IS NULL OR u.deleted = 0')
         ;
         $qb = $this->_applyFiltersClients($qb, $filters);
