db8f9529de68aaafed0ef21df6495ee16ede811e
bhola <singh.b78@gmail.com>
2019-09-19 16:04:48 +0530

    freelance search page

--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -193,20 +193,20 @@ class UserRepository extends ServiceEntityRepository
 
     private function _applyFiltersClients($qb, $filters){
         if($filters) {
-//            if (isset($filters["text"])) {
-//                if(trim($filters["text"]) != ''){
-//
-//                    if(isset($filters['skills']) or isset($filters["regions"])){
-//
-//                        $qb = $qb->orWhere("((packs.title LIKE :packText OR packs.description LIKE :packText)) OR CONCAT(u.firstname, ' ', u.lastname) LIKE :packText")->setParameter('packText','%'.$filters['text'].'%');
-//
-//                    }else{
-//
-//                        $qb = $qb->andWhere("((packs.title LIKE :packText OR packs.description LIKE :packText)) OR CONCAT(u.firstname, ' ', u.lastname) LIKE :packText")->setParameter('packText','%'.$filters['text'].'%');
-//
-//                    }
-//                }
-//            }
+            if (isset($filters["text"])) {
+                if(trim($filters["text"]) != ''){
+
+                    if(isset($filters['skills']) or isset($filters["regions"])){
+
+                        $qb = $qb->orWhere("((packs.title LIKE :packText OR packs.description LIKE :packText)) OR CONCAT(u.firstname, ' ', u.lastname) LIKE :packText")->setParameter('packText','%'.$filters['text'].'%');
+
+                    }else{
+
+                        $qb = $qb->andWhere("((packs.title LIKE :packText OR packs.description LIKE :packText)) OR CONCAT(u.firstname, ' ', u.lastname) LIKE :packText")->setParameter('packText','%'.$filters['text'].'%');
+
+                    }
+                }
+            }
 
             if(!isset($filters["text"]) || trim($filters["text"]) == '')
             {
