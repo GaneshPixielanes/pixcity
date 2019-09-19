e065a348e8f88e16d2460bc45331e85f98d3dfcd
bhola <singh.b78@gmail.com>
2019-09-19 17:49:29 +0530

    Mangopay kyc update

--- src/Controller/B2B/Client/CmClientMangopayCronController.php
+++ src/Controller/B2B/Client/CmClientMangopayCronController.php
@@ -215,7 +215,7 @@ class CmClientMangopayCronController extends Controller
 
                 $res = $mangoPayService->kycCreate($value->getMangopayUserId(), $filename);
                 //$mangoPayService->kycCreate($value->getMangopayUserId(),$filename1);
-
+                dd($res);
                 if(isset($res) != null ){
                     foreach($res as $v){
                         if(isset($v->Status) != null) {
@@ -223,7 +223,6 @@ class CmClientMangopayCronController extends Controller
                                 $value->setMangopayKycStatus("UNDER_VERIFICATION");
                             }
                             if($v->Status == "SUCCEEDED") {
-                                dd($v);
                                 $value->setMangopayKycStatus("SUCCESS");
                             }
                             if($v->Status == "REFUSED") {
