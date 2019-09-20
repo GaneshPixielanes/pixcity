954ce828f8c6029a1858562435c654370bce215f
bhola <singh.b78@gmail.com>
2019-09-20 15:42:02 +0530

    error check related to all user

--- src/Controller/Front/V2/CitymakerController.php
+++ src/Controller/Front/V2/CitymakerController.php
@@ -28,7 +28,7 @@ class CitymakerController extends SearchPageController
 
             $found = false;
             foreach($this->regions as &$region){
-                echo ($region['infos']->getId()).'<br>';
+                echo ($userRegion[0]->getId().'=='.$region['infos']->getId()).'<br>';
                 if($userRegion[0]->getId() === $region['infos']->getId()){
                     $region['pixies'][] = $user;
                     $found = true;
