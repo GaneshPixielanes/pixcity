03e806e1063a0efcf98def03995147128b5301d9
azim <azim@pix.city>
2019-11-15 18:14:06 +0530

    BanKAccountCOntroller testing

--- src/Controller/B2B/BankAccountController.php
+++ src/Controller/B2B/BankAccountController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller\B2B;
 
+use App\Repository\RoyaltiesRepository;
 use App\Repository\UserRepository;
 use App\Service\MangoPayService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -12,7 +13,7 @@ class BankAccountController extends AbstractController
     /**
      * @Route("/b2b/bank/account", name="b2b_bank_account")
      */
-    public function index(UserRepository $userRepository,MangoPayService $mangoPayService)
+    public function index(UserRepository $userRepository,MangoPayService $mangoPayService,RoyaltiesRepository $royaltiesRepository)
     {
         $em = $this->getDoctrine()->getManager();
 
@@ -20,8 +21,10 @@ class BankAccountController extends AbstractController
 
         foreach ($users as $user){
 
-            if($user->getMangopayUserId() != null || $user->getPixie()->getBilling()->getBillingIban() != null || $user->getPixie()->getBilling()->getBillingBic() != null ||
-                $user->getPixie()->getBilling()->getMangopayNeedToUpdate() != 1){
+            $check_royalties = $royaltiesRepository->findOneBy(['cm' => $user->getId()]);
+
+            if($user->getMangopayUserId() != null && $user->getPixie()->getBilling()->getBillingIban() != null && $user->getPixie()->getBilling()->getBillingBic() != null &&
+                $user->getPixie()->getBilling()->getMangopayNeedToUpdate() != 1 && $check_royalties != null){
 
                 $result = $mangoPayService->createBankAccount($user);
 
