735ca43279fb515bcea365fe0f9cc2b1d1bbb766
azim <azim@pix.city>
2019-11-21 15:38:34 +0530

    BankAccount and transfer controller updates

--- src/Controller/B2B/BankAccountController.php
+++ src/Controller/B2B/BankAccountController.php
@@ -30,7 +30,7 @@ class BankAccountController extends AbstractController
             if($user->getPixie() != null){
 
                 if($user->getMangopayUserId() != null && $user->getPixie()->getBilling()->getBillingIban() != null && $user->getPixie()->getBilling()->getBillingBic() != null &&
-                    $user->getPixie()->getBilling()->getMangopayNeedToUpdate() != 1 && $check_royalties == null && $user->getPixie()->getBilling()->getAddress()->getAddress() != null
+                    $user->getPixie()->getBilling()->getMangopayNeedToUpdate() != 1 && $check_royalties != null && $user->getPixie()->getBilling()->getAddress()->getAddress() != null
                     && $user->getPixie()->getBilling()->getAddress()->getCity() != null && $user->getPixie()->getBilling()->getAddress()->getCountry() != null
                     && $user->getPixie()->getBilling()->getAddress()->getZipcode()){
 
@@ -72,7 +72,7 @@ class BankAccountController extends AbstractController
 
         }
 
-        if(isset($missing) != null){
+        if(!empty($missing) != null){
 
             $mailer->send("rakesh@pix.city", 'IBAN and BIC details city-makers not proper or empty', 'emails/mangopay-bank-error-report.html.twig', [
                 'missingRecords' => $missing
